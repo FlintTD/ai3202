dab49ca764939c6292b695c5f68a4c294d72bffe
FlintTD <erha8359@colorado.edu>
2015-10-16 03:40:00 -0600

    revised version of MDP function with new helper functions.  Still does not work, but new methodology is being encoded.

--- Assignment5/MDP.py
+++ Assignment5/MDP.py
@@ -59,34 +59,30 @@ def ymax(val1, val2, val3, val4):
                 return val4, "left"
 
 
-def map_reward(map, state, hue, winstate):
-    if hue is True:
-        if (0 <= state.y < 8) and (0 <= state.x < 10):
-            if map[state.y][state.x] == (, -1):         # create a reward for the current state
-                return -1
-            elif map[state.y][state.x] == (, -2):
-                return -2
-            elif map[state.y][state.x] == (, None):
-                return 0
-        elif map[state.y][state.x] == 4:
-            reward += 1
-            reward = reward * (discount ^ step)
-            return reward, 0
-        elif (state.y == 0) and (state.x == 9):
-            reward += 50
-            reward = reward * (discount ^ step)
-            return reward, 2
+def map_reward(map, state):
+    if (0 <= state.y < 8) and (0 <= state.x < 10):
+        # return the reward
+        return map[state.y][state.x][1]
     else:
-        return 0, 1
+        return 0
+
+
+def map_utility(map, state):
+    if (0 <= state.y < 8) and (0 <= state.x < 10):
+        # return the reward
+        return map[state.y][state.x][0]
+    else:
+        return 0
+
 
 
 def MDP(map, new_map, cur_state, start, step, discount, trail, score, winfail, iterkill):
     state_value = map[start.y][start.x]
     if state_value is None:
         return None
-                                                        # check reward and winstate
-    # reward = map_reward(map, cur_state, rewutil, winfail)
-    reward = map[cur_state.y][cur_state.x][1]
+
+    # check reward
+    reward = map_reward(map, cur_state)
 
     if reward is None:
         return 0
@@ -95,14 +91,19 @@ def MDP(map, new_map, cur_state, start, step, discount, trail, score, winfail, i
     d_right = cur_state.tweak(0, 1)
     d_up = cur_state.tweak(-1, 0)
     d_left = cur_state.tweak(0, -1)
-                                                        # find the utility of each direction
-    u_down = 0.8 * map[d_down.y][d_down.x][0] + 0.1 * map[d_left.y][d_left.x][0] + 0.1 * map[d_right.y][d_right.x][0]
-    u_right = 0.8 * map[d_right.y][d_right.x][0] + 0.1 * map[d_down.y][d_down.x][0] + 0.1 * map[d_up.y][d_up.x][0]
-    u_up = 0.8 * map[d_up.y][d_up.x][0] + 0.1 * map[d_right.y][d_right.x][0] + 0.1 * map[d_left.y][d_left.x][0]
-    u_left = 0.8 * map[d_left.y][d_left.x][0] + 0.1 * map[d_up.y][d_up.x][0] + 0.1 * map[d_down.y][d_down.x][0]
-                                                        # find the best direction
+        # find the utility of each direction, (probability * (future value + reward))
+    u_down = (0.8 * (map_utility(map, d_down) + map_reward(map, d_down))) + (0.1 * (map_utility(map, d_left) + map_reward(map, d_left))) + (0.1 * (map_utility(map, d_right) + map_reward(d_right)))
+    u_right = (0.8 * (map_utility(map, d_right) + map_reward(map, d_right))) + (0.1 * (map_utility(map, d_down.y) + map_reward(map, d_down))) + (0.1 * (map_utility(map, d_up) + map_reward(map, d_up)))
+    u_up = (0.8 * (map_utility(map, d_up) + map_reward(map, d_up))) + (0.1 * (map_utility(map, d_right) + map_reward(map, d_right))) + (0.1 * (map_utility(map, d_left) + map_reward(map, d_left)))
+    u_left = (0.8 * (map_utility(map, d_left) + map_reward(map, d_left))) + (0.1 * (map_utility(map, d_up) + map_reward(map, d_up))) + (0.1 * (map_utility(map, d_down) + map_reward(map, d_down)))
+
+        # find the best direction
     y_max_value, direction = ymax(u_up, u_down, u_right, u_left)
-    max_utility = reward + y_max_value                  # find the state utility
+        # find the state utility
+    max_utility = reward + y_max_value
+        # read into new map
+    new_map[cur_state.y][cur_state.x][0] = (max_utility, reward, direction)
+
 
     pointer = None
     down_trail = trail
@@ -178,15 +179,20 @@ def main(argv):                                         # -------MAIN-----------
     for y in range(0, 7):
         for x in range(0, 9):
             if world[y][x] == 1:
-                world[y][x] = (-1, -1, None)
+                # mountains
+                world[y][x] = (0, -1, None)
             if world[y][x] == 2:
+                # walls
                 world[y][x] = (0, None, None)
             if world[y][x] == 3:
-                world[y][x] = (-2, -2, None)
+                # snakes
+                world[y][x] = (0, -2, None)
             if world[y][x] == 4:
-                world[y][x] = (1, 1, None)
+                # barns
+                world[y][x] = (0, 1, None)
             if world[y][x] == 50:
-                world[y][x] = (50, 50, None)
+                # apples
+                world[y][x] = (0, 50, None)
 
     hol = Node(7, 0)                                   # worldbuilding and execution
     start = Node(0, 9)
@@ -198,8 +204,9 @@ def main(argv):                                         # -------MAIN-----------
     leash = 1
 
     while leash > 0.5:
-        result = MDP(world, new_world, hol, start, steps, e, path, totalscore, sucess, leash)
+        MDP(world, new_world, hol, start, steps, e, path, totalscore, sucess, leash)
 
+    '''
     sum = 0
     for r in range(0, len(result[1])-1):
         sum += result[1][r]
@@ -208,7 +215,7 @@ def main(argv):                                         # -------MAIN-----------
         yy.print_node()
     print "Total F-Score is: %s" % sum
     print "Total number of unique node checks: %s" % result[2]
-
+    '''
 
 if __name__ == "__main__":
     main(sys.argv)
